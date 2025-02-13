5219eb6792c89d6c8699a6c8ea6695da2b605872
Adityagupta0973 <adityag0908@gmail.com>
2025-02-13 23:39:24 +0530

    Initial Commit

--- crud/settings.py
+++ crud/settings.py
@@ -119,11 +119,17 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
